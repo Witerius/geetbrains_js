b0a351bab6d8fc8ba76689ec221c2584d647119f
mikhail <wizardmb@gmail.com>
2021-04-07 22:50:30 +0600

    доработал 1-й урок

--- gb_golang_2/hw_1/hw_1_1.go
+++ gb_golang_2/hw_1/hw_1_1.go
@@ -1,25 +1,36 @@
 package hw_1
 
 import (
-  "fmt"
-  "os"
-  "strconv"
+	"fmt"
+	"runtime/debug"
 )
 
 func main() {
 
-  defer func() {
-    if v := recover(); v != nil {
-      fmt.Println("recovered", v)
-    }
-  }()
+	defer func() {
+		if v := recover(); v != nil {
+			fmt.Println("recovered", v)
+			err := New("error!")
+			fmt.Println(err)
+		}
+	}()
 
-panik
+	panic("AAA!!")
 
 }
 
 type ErrorE struct {
-    text string
+	text  string
+	trace string
 }
 
+func (e ErrorE) Error() string {
+	panic("implement me")
+}
 
+func New(text string) error {
+	return &ErrorE{
+		text:  text,
+		trace: string(debug.Stack()),
+	}
+}
